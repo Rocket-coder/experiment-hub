51fde4eed1b1161843fcb3978195277e40cbf6e4
Rocket-coder <komarovo203@gmail.com>
2026-09-21 22:39:20 +0300

    project check to service

--- src/experiment_hub/service.py
+++ src/experiment_hub/service.py
@@ -2,7 +2,7 @@ from uuid import UUID
 from datetime import datetime, timezone
 
 from experiment_hub.models import Run, RunUpdate
-from experiment_hub.storage import get_run, update_run_record
+from experiment_hub.storage import get_run, update_run_record, get_project
 
 
 class RunNotFoundError(Exception):
@@ -10,6 +10,10 @@ class RunNotFoundError(Exception):
 
 
 class RunAlreadyFinishedError(Exception):
+    pass
+
+
+class ProjectNotFoundError(Exception):
     pass
 
 
@@ -29,3 +33,12 @@ def update_run(run_id: UUID, run_update: RunUpdate) -> Run:
     update_run_record(run)
 
     return run
+
+
+def check_project(project_id: UUID):
+    project = get_project(project_id)
+
+    if project is None:
+        raise ProjectNotFoundError()
+
+    
