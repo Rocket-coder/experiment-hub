76e3ca619772d659e5cd4acdbcf94aadf44c08de
Rocket-coder <komarovo203@gmail.com>
2026-09-21 15:38:08 +0300

    fix

--- src/experiment_hub/service.py
+++ src/experiment_hub/service.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 from datetime import datetime, timezone
 
-from experiment_hub.models import Run, RunUpdate, RunCreate
+from experiment_hub.models import Run, RunUpdate
 from experiment_hub.storage import runs
 
 
@@ -15,10 +15,10 @@ class RunAlreadyFinishedError(Exception):
 
 def update_run(run_id: UUID, run_update: RunUpdate) -> Run:
     if run_id not in runs:
-        raise RunNotFoundError
+        raise RunNotFoundError()
 
     if runs[run_id].status != "running":
-        raise RunAlreadyFinishedError
+        raise RunAlreadyFinishedError()
 
     runs[run_id].status = run_update.status
     runs[run_id].results = run_update.results
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,8 +1,69 @@
-from tests.test_runs import test_complete_run, test_patch_finished_run
+import pytest
+
+from uuid import uuid4
+from datetime import datetime, timezone
+
+from experiment_hub.models import Run, RunUpdate
+from experiment_hub.service import update_run, RunAlreadyFinishedError
+from experiment_hub.storage import runs
+
+
+@pytest.fixture(autouse=True)
+def clear_runs():
+    runs.clear()
+    yield
+    runs.clear()
+
 
 def test_update_running_to_completed():
-    test_complete_run()
+    run = Run(
+        run_id = uuid4(),
+        status = "running",
+        start_time = datetime.now(timezone.utc),
+        program = "unit_test.py",
+        parameters = {
+            "param1": 25,
+            "param2": 5.1
+        }
+    )
+    runs[run.run_id] = run
+
+    update = RunUpdate(
+        status="completed",
+        results="ok"
+    )
+
+    updated_run = update_run(run.run_id, update)
+
+    assert updated_run.status == "completed"
+    assert updated_run.results == "ok"
+    assert updated_run.end_time is not None
 
 
 def test_try_to_update_finished_run():
-    test_patch_finished_run()
+    run = Run(
+            run_id = uuid4(),
+            status = "running",
+            start_time = datetime.now(timezone.utc),
+            program = "unit_test.py",
+            parameters = {
+                "param1": 25,
+                "param2": 5.1
+            }
+        )
+    runs[run.run_id] = run
+
+    update = RunUpdate(
+        status="completed",
+        results="ok"
+    )
+
+    update_run(run.run_id, update)
+
+    update = RunUpdate(
+        status="failed",
+        results="Bad request"
+    )
+
+    with pytest.raises(RunAlreadyFinishedError):
+        update_run(run.run_id, update)
