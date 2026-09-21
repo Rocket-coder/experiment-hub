4b0796a94188cd846e2994c726720b98e7dc8e88
Rocket-coder <komarovo203@gmail.com>
2026-09-21 17:38:01 +0300

    fix

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from datetime import datetime, timezone
 
 from experiment_hub import storage
-from experiment_hub.storage import init_db, save_run, get_run
+from experiment_hub.storage import save_run, get_run
 from experiment_hub.models import Run
 
 def test_init_and_save_runs_with_get():
