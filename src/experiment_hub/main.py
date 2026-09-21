54f10463c87f2d4fffe54d9b68da8048c61ac3cd
Rocket-coder <komarovo203@gmail.com>
2026-09-21 13:17:33 +0300

    add_get_post_run

--- src/experiment_hub/main.py
+++ src/experiment_hub/main.py
@@ -1,4 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from uuid import uuid4, UUID
+from datetime import datetime, timezone
+
+class RunCreate(BaseModel):
+    program: str
+    parameters: dict[str, int | float]
+
+class Run(BaseModel):
+    run_id: UUID
+    status: str
+    start_time: datetime
+    program: str
+    parameters: dict[str, int | float]
+    results: str | None = None
+    end_time: datetime | None = None
+
 
 app = FastAPI()
 
@@ -9,3 +26,29 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+runs: dict[UUID, Run] = {}
+
+@app.post("/runs", status_code=201)
+async def create_run(run_create: RunCreate):
+    run = Run(
+        run_id = uuid4(),
+        status = "running",
+        start_time = datetime.now(timezone.utc),
+        program = run_create.program,
+        parameters = run_create.parameters
+    )
+
+    runs[run.run_id] = run
+    
+    return run
+
+@app.get("/runs/{run_id}")
+async def get_run(run_id: UUID):
+    if run_id not in runs:
+        raise HTTPException(
+            status_code=404,
+            detail="Run not found"
+        )
+
+    return runs[run_id]
---
+++ tests/test_runs.py
@@ -0,0 +1,75 @@
+import pytest
+from fastapi.testclient import TestClient
+from experiment_hub.main import app, runs
+from uuid import uuid4
+
+@pytest.fixture(autouse=True)
+def clear_runs():
+    runs.clear()
+
+    yield
+
+    runs.clear()
+
+
+client = TestClient(app)
+
+
+def create_run():
+    body = {
+            "program": "test_post.py",
+            "parameters": {
+                "learning_rate": 0.21,
+                "batch_size": 64
+            }
+        }
+
+    response = client.post("/runs", json=body)
+
+    assert response.status_code == 201
+
+    created_run = response.json()
+    
+    assert created_run["status"] == "running"
+    assert created_run["program"] == "test_post.py"
+    assert created_run["parameters"] == body["parameters"]
+    assert created_run["results"] is None
+    assert created_run["end_time"] is None
+
+    return created_run
+
+def test_create_and_get_run():
+    created_run = create_run()
+
+    run_id = created_run["run_id"]
+
+    get_response = client.get(f"/runs/{run_id}")
+
+    assert get_response.status_code == 200
+    assert get_response.json() == created_run
+
+def test_run_post_without_parameters():
+    bad_body = {
+        "program": "no_params.py"
+    }
+
+    response = client.post("/runs", json=bad_body)
+
+    assert response.status_code == 422
+
+
+def test_run_get_invalid_uuid():
+    response = client.get("/runs/123")
+
+    assert response.status_code == 422
+
+def test_run_get_unknown_uuid():
+    run_id = uuid4()
+
+    response = client.get(f"/runs/{run_id}")
+
+    assert response.status_code == 404
+
+    data = response.json()
+
+    assert data["detail"] == "Run not found"
